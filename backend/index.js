3eedecc3cf1c763b1ea29a7298491c65bf9ecf5b
Luis <luisrc91@live.com>
2024-02-08 09:15:19 -0500

    created middleware

--- backend/index.js
+++ backend/index.js
@@ -22,8 +22,16 @@ app.listen(3000, () => {
   console.log("listening on 30000");
 });
 
-app.use('/api/user', userRouter);
-app.use('/api/auth', authRouter);
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
 
-
-
+// middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
