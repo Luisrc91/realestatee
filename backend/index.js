0e5973363986d57e69c07099e5e10b771fa7ec73
Luis <luisrc91@live.com>
2024-02-08 07:16:02 -0500

    connected to mongo

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connect to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
 app.listen(300, () => {
-    console.log('listening on 30000');
+  console.log("listening on 30000");
 });
+
