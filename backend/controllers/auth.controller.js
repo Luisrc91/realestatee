1ccc709731a3abcf724cec27b475231d12e69a1e
Luis <luisrc91@live.com>
2024-02-08 13:41:33 -0500

    haspassword

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -22,8 +22,12 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(404, "User not Found!"));
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Wrong Credentials!"));
-    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
-    res.cookie('access_token', token, { httpOnly: true }).status(200).json(validUser)
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+    const { password: pass, ...rest} = validUser._doc;
+    res
+    .cookie('access_token', token, { httpOnly: true })
+    .status(200)
+    .json(rest);
   } catch (err) {
     next(err);
   }
