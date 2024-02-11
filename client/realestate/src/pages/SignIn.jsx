da43c684037368765f7dc75f0dfad9dce7a24490
Luis <luisrc91@live.com>
2024-02-11 16:02:34 -0500

    added redux to sign in

--- client/realestate/src/pages/SignIn.jsx
+++ client/realestate/src/pages/SignIn.jsx
@@ -1,6 +1,94 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/useSlice";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const { loading, error } = useSelector((state) => state.user);
+  // const [error, setError] = useState(null);
+  // const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  // console.log(formData);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      // setLoading(true);
+      dispatch(signInStart());
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(signInFailure(data.message));
+        // setLoading(false);
+        // setError(data.message);
+        return;
+      }
+      dispatch(signInSuccess(data));
+      // setLoading(false);
+      // setError(null);
+      navigate("/");
+    } catch (e) {
+      dispatch(signInFailure(e.message));
+      // setLoading(false);
+      // setError(e.message);
+    }
+  };
+  // console.log(formData);
   return (
-    <div>SignIn</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        {/* <input
+          type="text"
+          placeholder="username"
+          className="border p-3 rounde-lg"
+          id="username"
+          onChange={handleChange}
+        /> */}
+        <input
+          type="text"
+          placeholder="email"
+          className="border p-3 rounde-lg"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="border p-3 rounde-lg"
+          id="password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont have an Account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign Up</span>
+        </Link>
+      </div>
+      {error && <p className="text-red-500 mt-5">{error}</p>}
+    </div>
+  );
 }
