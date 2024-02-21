06f6a70ecea8fdd02a9df7d092ec6e389f9827db
Luis <luisrc91@live.com>
2024-02-21 11:02:08 -0500

    fix error

--- client/realestate/src/pages/CreateListing.jsx
+++ client/realestate/src/pages/CreateListing.jsx
@@ -1,6 +1,9 @@
 // import React from 'react'
 
+
 export default function CreateListing() {
+const [files, setFiles] = useState([])
+
   return (
     <main className="p-3 max-w-4xl mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">
@@ -114,11 +117,21 @@ export default function CreateListing() {
             </span>
           </p>
           <div className="flex gap-4">
-            <input className="p-3 border-gray-300 rounded w-full" type="file" id="images" accept="imgages/*" multiple />
-            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disable:opacity-80">Upload</button>
+            <input onChange={(e)=>setFiles(e.target.files}
+              className="p-3 border-gray-300 rounded w-full"
+              type="file"
+              id="images"
+              accept="imgages/*"
+              multiple
+            />
+            <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disable:opacity-80">
+              Upload
+            </button>
           </div>
 
-        <button className="p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 bg-slate-700 text-white">Create Listing</button>
+          <button className="p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 bg-slate-700 text-white">
+            Create Listing
+          </button>
         </div>
       </form>
     </main>
