e6013c81c7d7c5eab4d48768aefdf3881b67d797
Luis <luisrc91@live.com>
2024-02-23 10:43:16 -0500

    get userlisting route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import bcryptjs from "bcryptjs";
 import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
+import Listing from "../models/listing.model.js";
 
 export const test = (req, res) => {
   res.json({
@@ -42,8 +43,8 @@ export const deleteUser = async (req, res, next) => {
     return next(errorHandler(401, "You can only delete your own account!"));
   try {
     await User.findByIdAndDelete(req.params.id);
-    res.clearCookie('access_token');
-    res.status(200).json("User deleted successfully")
+    res.clearCookie("access_token");
+    res.status(200).json("User deleted successfully");
   } catch (error) {
     next(error);
   }
@@ -51,10 +52,22 @@ export const deleteUser = async (req, res, next) => {
 
 export const signOut = async (req, res, next) => {
   try {
-    res.clearCookie('access_token');
+    res.clearCookie("access_token");
     res.status(200).json("User signed out successfully");
   } catch (error) {
     next(error);
-    
   }
-}
+};
+
+export const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, 'You can only view your own listings!'));
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,12 +1,19 @@
-import express from 'express';
-import {deleteUser, signOut, test, updateUser } from '../controllers/user.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+import express from "express";
+import {
+  deleteUser,
+  signOut,
+  test,
+  updateUser,
+  getUserListings,
+} from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get('/test', test); 
-router.post('/update/:id', verifyToken, updateUser);
-router.delete('/delete/:id', verifyToken, deleteUser);
-router.get('/signout', signOut);
+router.get("/test", test);
+router.post("/update/:id", verifyToken, updateUser);
+router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/signout", signOut);
+router.get('/listings/:id', verifyToken, getUserListings)
 
 export default router;
