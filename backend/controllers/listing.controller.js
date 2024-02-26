46bd9b98fe96e2d51c23861f6e66fdbb3f65ed9b
Luis <luisrc91@live.com>
2024-02-26 11:23:18 -0500

    update listing route

--- backend/controllers/listing.controller.js
+++ backend/controllers/listing.controller.js
@@ -19,8 +19,28 @@ export const deleteListing = async (req, res, next) => {
     return next(errorHandler(401, "You can only delete your own listing"));
   }
   try {
-    await Listing.findByIdAndDelete(req.params.id)
-    res.status(200).json('Listing has been deleted!')
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing has been deleted!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params.id);
+  if (!listing) {
+    return next(errorHandler(404, "Listing not found!"));
+  }
+  if (req.user.id !== listing.userRef) {
+    return next(errorHandler(404, "You can only update your own listing!"));
+  }
+  try {
+    const updateListing = await Listing.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updateListing);
   } catch (error) {
     next(error);
   }
