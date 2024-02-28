6b90523a16b3095d898ad725e5d7831bc5afc5d9
Luis <luisrc91@live.com>
2024-02-28 10:29:17 -0500

    landlord email feauture complete

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -68,6 +68,17 @@ export const getUserListings = async (req, res, next) => {
       next(error);
     }
   } else {
-    return next(errorHandler(401, 'You can only view your own listings!'));
+    return next(errorHandler(401, "You can only view your own listings!"));
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) return next(errorHandler(400, "User not found"));
+    const { password: pass, ...rest } = user._doc;
+    res.status(200).json(rest);
+  } catch (err) {
+    next(error);
   }
 };
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -5,6 +5,7 @@ import {
   test,
   updateUser,
   getUserListings,
+  getUser
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -15,5 +16,7 @@ router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 router.get("/signout", signOut);
 router.get('/listings/:id', verifyToken, getUserListings)
+
+router.get('/:id', verifyToken, getUser )
 
 export default router;
