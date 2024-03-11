f0e059eca3ba17758988f20337d4ca6afc2ab525
Luis <luisrc91@live.com>
2024-03-11 12:03:54 -0400

    fixed lisitin card on search

--- client/realestate/src/pages/Search.jsx
+++ client/realestate/src/pages/Search.jsx
@@ -204,11 +204,10 @@ export default function Search() {
         </form>
       </div>
       <div className="flex-1">
-        <h1
-          className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
+        <h1 className="text-3xl font-semibold border-b p-3 text-slate-700 mt-5">
           Listing Results:
         </h1>
-        <div className="p-7  flex-wrap gap-4">
+        <div className="p-7 flex flex-wrap gap-4">
           {!loading && listings.length === 0 && (
             <p className="text-xl text-slate-700"> No listing found!</p>
           )}
